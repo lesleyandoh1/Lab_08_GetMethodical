59630821d7a39a90f71d9c87558e568bfe3e5e7c
lesleyandoh <ackahalj@mail.uc.edu>
2025-07-10 16:26:24 -0400

    SafeInputWork

--- src/DevTest.java
+++ src/DevTest.java
@@ -4,7 +4,7 @@ public class DevTest{
 public static void main(String[] args) {
      Scanner input = new Scanner(System.in);
 
-//    SafeInput.getNonZeroLenString(input, "Enter a string: ");
+    SafeInput.getNonZeroLenString(input, "Enter a string: ");
     SafeInput.getRangedInt(input, "Enter a number between the range 1-12", 1, 12);
 
 
